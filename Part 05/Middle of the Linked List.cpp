54846eb68a683d61d474038b3be09c22b8cabc80
Aman Sharma <aman703jk@gmail.com>
2022-08-21 18:39:45 +0530

    Middle of the Linked List

---
+++ Part 05/Middle of the Linked List.cpp
@@ -0,0 +1,51 @@
+// PROBLEM 
+// https://leetcode.com/problems/middle-of-the-linked-list/
+
+
+/*
+    Solution 1 : Naive Method
+    Time complexity : O(N) + O(N/2) ~ O(N)
+    Space Complexity : O(1)
+*/
+
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int n = 0;
+    	ListNode* temp = head;
+    	while(temp) {
+        	n++;
+        	temp = temp->next;
+    	}
+   	 
+    	temp = head;
+   	 
+    	for(int i = 0; i < n / 2; i++) {
+        	temp = temp->next;
+    	}
+   	 
+    	return temp;
+    }
+};
+
+
+
+
+/*
+    Solution 2 : 2 Pointer approach ||  Tortoise-Hare-Approach
+    Time complexity : O(N) 
+    Space Complexity : O(1)
+*/
+
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
